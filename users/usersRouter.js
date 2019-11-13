d2f8a137a3a726c070a4129becec5b1785ed2f20
Funmi7 <talabifunmilayo@gmail.com>
2019-11-13 17:41:33 +0100

    All endpoints working

---
+++ users/usersRouter.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const Users = require('./usersModel');
+const restricted = require('../auth/restrictedMiddleware');
+
+router.get('/', restricted, (req, res) => {
+  Users.find()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  });
+});
+
+module.exports = router;
